39f88f3ee320ccd337782fe45052c9c3ba716fbe
luckify <luckin@fastmail.com>
2024-05-05 22:20:16 +0800

    update curl build script

--- examples/curl/build.sh
+++ examples/curl/build.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-[ ! -d curl ] && git clone --depth=1 https://github.com/cntrump/curl.git
+[ ! -d curl ] && git clone --depth=1 https://github.com/curl/curl.git
 
 [ -d build ] && rm -rf build
 
+SDK_SYSROOT=$(xcrun --sdk macosx --show-sdk-path)
+
 cmake -S curl -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=$(pwd)/../../xcrun.toolchain.cmake \
         -DCMAKE_OSX_TRIPLE_OS=macosx \
-        -DCMAKE_OSX_TRIPLE_OS_VERSION=10.13 \
+        -DCMAKE_OSX_TRIPLE_OS_VERSION=10.9 \
         -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
         -DBUILD_SHARED_LIBS=OFF \
         -DCURL_USE_SECTRANSP=ON \
@@ -16,6 +18,7 @@ cmake -S curl -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=$(pwd)/../../xcrun.toolch
         -DENABLE_IPV6=ON \
         -DCURL_USE_LIBPSL=OFF \
         -DCURL_DISABLE_LDAP=ON \
-        -DCURL_USE_SYSROOT_LIBZ=ON
+        -DZLIB_INCLUDE_DIR=${SDK_SYSROOT}/usr/include \
+        -DZLIB_LIBRARY=${SDK_SYSROOT}/usr/lib/libz.tbd
 
 ninja -C build
