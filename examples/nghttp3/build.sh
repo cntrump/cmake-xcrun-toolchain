f63ce00c113de8aaf1ea3b1b40bfa96515bb2ce9
luckify <luckin@fastmail.com>
2024-05-06 21:26:55 +0800

    Add nghttp3 build script

---
+++ examples/nghttp3/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -d nghttp3 ]; then
+  git clone -b v1.2.0 https://github.com/ngtcp2/nghttp3.git
+  pushd nghttp3
+  git submodule update --init
+  popd
+fi
+
+[ -d build ] && rm -rf build
+
+cmake -S nghttp3 -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=$(pwd)/../../xcrun.toolchain.cmake \
+        -DCMAKE_OSX_TRIPLE_OS=macosx \
+        -DCMAKE_OSX_TRIPLE_OS_VERSION=10.9 \
+        -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+        -DENABLE_LIB_ONLY=ON \
+        -DENABLE_SHARED_LIB=OFF \
+        -DENABLE_STATIC_LIB=ON
+
+ninja -C build
